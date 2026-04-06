30f40469213c0fce15484805366dca859c1b54f3
akshay pawar <ap3690700@gmail.com>
2026-04-07 00:18:15 +0530

    refactor: rename prisma client export and update user onboarding logic
    - Changed export name from `prisma` to `db` in lib/db.js for clarity.
    - Added new user onboarding functionality in modules/auth/actions/index.js using Clerk's currentUser.
    - Updated package.json to downgrade Prisma dependencies to version 6.
    - Removed unnecessary prisma.config.ts and migration files.
    - Updated schema.prisma to define User model with additional fields and enum for user roles.

---
+++ modules/auth/actions/index.js
@@ -0,0 +1,50 @@
+"use server";
+
+import {db} from "@/lib/db"
+import {currentUser} from "@clerk/nextjs/server"
+
+export const onBoardUser = async()=>{
+    try {   
+        const user = await currentUser();
+
+        if(!user){
+            return {
+                success:false,error: "No authenticated user found"
+            }
+        }
+
+        const {id,firstName,lastName,imageUrl,emailAddresses}=user;
+
+        const newUser=db.user.upsert({
+            where:{
+                clerkId:id
+            },
+            update:{
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0]?.emailAddress || ""
+            },
+            create:{
+                clerkId:id,
+                firstName: firstName || null,
+                lastName: lastName || null,
+                imageUrl: imageUrl || null,
+                email: emailAddresses[0]?.emailAddress || ""
+            }
+        });
+
+        return {
+            success: true,
+            user:newUser,
+            message:"User onBoarded Successfully"
+        }
+         
+    } catch (error) {
+        console.error("Error onBoardong user:",error);
+        return {
+            success: false,
+            error: "Failed to onboard user"
+        }
+    }
+}
