22f4ebf3d698281b28a9f50397561b858577de0d
akshay pawar <ap3690700@gmail.com>
2026-04-07 16:57:26 +0530

    fix: update font settings to use Poppins for better typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider"
+import { Poppins } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Leetcode",
   description: "Leetcode clone project",
 };
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({
   children,
@@ -18,7 +25,7 @@ export default function RootLayout({
       <html
         lang="en"
         suppressHydrationWarning
-        className={`h-full antialiased`}
+        className={` ${poppins.variable} h-full antialiased`}
       >
         <body className="min-h-full flex flex-col">
           <ThemeProvider
